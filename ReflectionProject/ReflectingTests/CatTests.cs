3a17b8a181594cedc4c09cd9c37dec7e892a3be1
coraw24 <cwiersema@students.ccu.edu>
2018-11-07 19:44:25 -0700

    Final

---
+++ ReflectionProject/ReflectingTests/CatTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reflections;
+
+namespace ReflectingTests
+{
+    [TestClass]
+    public class CatTests
+    {
+        [TestMethod]
+        public void CatConstructionTest()
+        {
+            var cat = new Cat();
+
+            var reflectCat = Activator.CreateInstance(typeof(Cat)) as Cat;
+
+            Assert.IsNotNull(cat);
+            Assert.IsNotNull(reflectCat);
+            Assert.IsInstanceOfType(reflectCat, typeof(Cat));
+
+        }
+
+        [TestMethod]
+        public void CatPropertyTest()
+        {
+            var dog = new Cat();
+
+            var reflectCat = Activator.CreateInstance(typeof(Cat)) as Cat;
+
+            reflectCat.GetType().GetProperty("Lives").SetValue(reflectCat, 3);
+            reflectCat.GetType().GetProperty("Breed").SetValue(reflectCat, "Stray");
+            Assert.AreEqual(3, reflectCat.GetType().GetProperty("Lives").GetValue(reflectCat, null));
+            Assert.AreEqual("Stray", reflectCat.GetType().GetProperty("Breed").GetValue(reflectCat, null));
+
+            Assert.IsTrue(reflectCat.GetType().GetProperty("Lives").CanRead);
+            Assert.IsTrue(reflectCat.GetType().GetProperty("Lives").CanWrite);
+
+            Assert.IsTrue(reflectCat.GetType().GetProperty("Breed").CanRead);
+            Assert.IsTrue(reflectCat.GetType().GetProperty("Breed").CanWrite);
+        }
+
+        [TestMethod]
+        public void CatMethodTest()
+        {
+            var cat = new Cat();
+
+            var reflectCat = cat.GetType().GetMethod("LoseLife");
+
+
+            int lives = (int)reflectCat.Invoke(cat, new object[] { 3 });
+
+            Assert.AreEqual(2, lives);
+
+        }
+    }
+
+}
+
---
+++ ReflectionProject/ReflectingTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReflectingTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReflectingTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8e113e8b-afa0-4c30-8fda-b3a16177ff63")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
